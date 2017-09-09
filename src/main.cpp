d2c6283bd1e011529d9eec9aca16c2794ac39939
LeoDJ <leandro.spaeth@gmail.com>
2017-09-10 00:46:17 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("\n\nArtnet2DMX");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
