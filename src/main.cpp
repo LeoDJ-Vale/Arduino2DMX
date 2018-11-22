a100f388fcbc5dd736da6bcff3629e9f773ef58e
LeoDJ <leandro.spaeth@gmail.com>
2018-11-23 00:55:30 +0100

    fix glitching by requesting ArtNet packet only once && code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 #include <Arduino.h>
-//#include <DMXSerial.h>
+
+#define DEBUG false
+
 #include <DmxSimple.h>
 
 #include <ArtnetEthercard.h>
 ArtnetEthercard artnet;
-/*#include <Artnet.h>
-#include <SPI.h>
-Artnet artnet;*/
 
-#define DEBUG true
-//SoftwareSerial sSerial (4, 5); //RX, TX
 
 const int universe = 1; //DMX universe to be translated
 byte ip[] = {192, 168, 178, 97};
@@ -18,56 +15,76 @@ byte mac[] = {0x04, 0xE9, 0xE5, 0x00, 0x69, 0xEC}; //
 void callback(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len) 
 {
     //have to use this workaround due to c++ limitations (no std::bind in arduino and can't call functions of objects from static function (no this pointer))
-    //Serial.print(".");
+
+    #if DEBUG
+        Serial.print("Raw UDP Packet: ");
+        for(uint16_t i = 0; i < min(64, len); i++) {
+            Serial.print((uint8_t)data[i]);
+            Serial.print(" ");
+        }
+        Serial.println();
+    #endif
+
     artnet._udpCallback(dest_port, src_ip, src_port, data, len);
 }
 
 void setup() {
-    //sSerial.begin(19200);
-    //sSerial.println("\n\nArtnet2DMX");
     Serial.begin(115200);
     Serial.println("\n\nArtnet2DMX");
     artnet.begin(mac, ip); //ip not needed for dhcp
     ether.udpServerListenOnPort(callback, ART_NET_PORT); //workaround
 
-
-    //artnet.setCallback(artnet._udpCallback);
-    //Serial.println(Ethernet.localIP()); //print dhcp IP
     ether.printIp("My IP: ", ether.myip);
-    //DMXSerial.init(DMXController);
-    //DMXSerial.maxChannel(16);
-    //DMXSerial.write(1, 255);
+    
     DmxSimple.usePin(3);
     DmxSimple.maxChannel(512);
 }
-unsigned long lastPacket = 0;
+unsigned long lastPacket = 0, lastStatusPrint = 0;
 bool blinkState = 0;
+uint8_t packetCount = 0;
 void loop() {
     if (artnet.read() == ART_DMX)
     {
         lastPacket = millis();
         if(artnet.getUniverse() == universe)
         {
-            //Serial.println(universe);
+            uint8_t *artnetFrame = artnet.getDmxFrame();
+            packetCount++;
+
             for (uint16_t i = 0 ; i < artnet.getLength() ; i++)
             {
-              byte val = artnet.getDmxFrame()[i];
-              //DMXSerial.write(i, val);
-              DmxSimple.write(i, val);
-              if(i < 8) {
-                Serial.print(val);
-                Serial.print(" ");
-              }
+              byte val = artnetFrame[i];
+
+              DmxSimple.write(i + 1, val);
+              
+              #if DEBUG
+                if(i < 8) {
+                    Serial.print(val);
+                    Serial.print(" ");
+                }
+              #endif
             }
-            Serial.println();
+            #if DEBUG
+                Serial.println();
+            #endif
         }
+        artnet.doneProcessing();
     }
 
     if(millis() - lastPacket >= 1000) {
         lastPacket = millis();
         blinkState = !blinkState;
-        //DMXSerial.write(1, blinkState ? 255 : 0);
+
         DmxSimple.write(1, blinkState ? 255 : 0);
-        Serial.println(blinkState);
+        DmxSimple.write(5, blinkState ? 255 : 0);
+
+        Serial.println(F("ERROR: No Art-Net Packets received for over 1 second."));
+    }
+
+    if(millis() - lastStatusPrint >= 1000) {
+        Serial.print(((uint32_t)packetCount * 1000) / (millis() - lastStatusPrint));
+        Serial.println(" packets / s");
+        packetCount = 0;
+        lastStatusPrint = millis();
     }
 }
